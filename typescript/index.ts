75cee31c82b4ebe61b49dc44171de9ea49be0a5f
Aditya Mayukh Som <adi.kg2@gmail.com>
2023-04-21 19:44:27 +0530

    right move working

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,1 +1,15 @@
-console.log("this is gg")
+const player = document.getElementById('player') as HTMLDivElement;
+
+window.addEventListener("keypress", (event) => {
+
+    if (event.key == 'w')
+        console.log('forward');
+    else if (event.key == 's') {
+        console.log('backward');
+        player.style.left = player.offsetLeft + 2 + "px";
+    }
+    else if (event.key == 'a')
+        console.log('right');
+    else if (event.key == 'd')
+        console.log('left');
+});
